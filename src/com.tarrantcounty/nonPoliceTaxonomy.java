f9c9e51f2ce7bb71493bf422953699b7b55c5317
Dr. Dr. Prof. h.c. Jason Nygel Petersson <51761071+meja0016@users.noreply.github.com>
2023-11-22 15:29:44 -0600

    Create nonPoliceTaxonomy.java

---
+++ src/com.tarrantcounty/nonPoliceTaxonomy.java
@@ -0,0 +1,12 @@
+public class nonPoliceTaxonomy{
+
+        public void nonPolicePicturesTaxonomy(){
+        System.out.print("IyEvYmluL2Jhc2gKCgoKZm9yIGkgaW4gYGxzIG5vblBvbGljZVBpY3R1cmVzYDsgZG8KICBlY2hvICJlY2hvICRpIiA+PiAxLnNoCmlmIFsgJGkgPT0gJzA3LTI4LTIwMjMtYi1pbWFnZTE5NS5qcGcnIF07IHRoZW4gZXhpdCAwOyBmaQogIGNhdCBub25Qb2xpY2VBSS5zaCB8IHNlZCAgInMvMDItMTgtMjAyMy1pbWFnZTFcLnBuZy8kaS9nIiA+PiAxLnNoCmRvbmUKZWNobyAiIyEvYmluL2Jhc2giID4gMS5zaAoK");
+        }
+
+        public static void main(String[] args){
+                new nonPolice().nonPolicePicturesTaxonomy();
+
+        }
+
+}
